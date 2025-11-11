79e96100866f3e1fb1d5acfb8215d62c81281299
Caio Iacopini <caio.iansen@gmail.com>
2025-11-11 16:51:16 -0500

    fix

--- backend/server.js
+++ backend/server.js
@@ -454,6 +454,19 @@ app.delete("/ranking/reset", auth, isAdmin, async (req, res) => {
   }
 });
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve build do React
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 app.listen(3000, () => {
   console.log("API rodando na porta 3000");
 });
