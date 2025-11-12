4f8ddbdfe687008d8c1c4ddba7354631bed481cf
Caio Iacopini <caio.iansen@gmail.com>
2025-11-12 18:42:40 -0500

    V 0.3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:3000" // quando rodar Vite em dev
+    : window.location.origin, // quando estiver buildado no Render
 });
 
 // sempre que tiver token no localStorage, envia
