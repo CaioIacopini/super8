c25ed2f65db3588d0a37753b4dbcf91f00b6c5e2
Caio Iacopini <caio.iansen@gmail.com>
2025-11-12 17:51:47 -0500

    V 0.2

--- backend/server.js
+++ backend/server.js
@@ -462,6 +462,9 @@ const __dirname = path.dirname(__filename);
 
 // serve build do React
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
